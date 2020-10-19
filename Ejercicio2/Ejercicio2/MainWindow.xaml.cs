60f3af8e230b280363cd79dba998f1b344173744
AlejandroOrtizBalmis <57657339+AlejandroOrtizBalmis@users.noreply.github.com>
2020-10-19 19:06:12 +0200

    Add files via upload

---
+++ Ejercicio2/Ejercicio2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+
+namespace Ejercicio2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void limpiarButton_Click(object sender, RoutedEventArgs e)
+        {
+            operadorTextBox.Text = "";
+            operando1TextBox.Text = "";
+            operando2TextBox.Text = "";
+            resultadoTextBox.Text = "";
+        }
+
+        private void calcularButton_Click(object sender, RoutedEventArgs e)
+        {
+            switch (operadorTextBox.Text)
+            {
+                case "+":
+                    resultadoTextBox.Text = (double.Parse(operando1TextBox.Text) + double.Parse(operando2TextBox.Text)).ToString();
+                    break;
+                case "-":
+                    resultadoTextBox.Text = (double.Parse(operando1TextBox.Text) - double.Parse(operando2TextBox.Text)).ToString();
+                    break;
+                case "*":
+                    resultadoTextBox.Text = (double.Parse(operando1TextBox.Text) * double.Parse(operando2TextBox.Text)).ToString();
+                    break;
+                case "/":
+                    resultadoTextBox.Text = (double.Parse(operando1TextBox.Text) / double.Parse(operando2TextBox.Text)).ToString();
+                    break;
+                default:
+                    resultadoTextBox.Text = "ERROR";
+                    break;
+            }
+        }
+    }
+}
